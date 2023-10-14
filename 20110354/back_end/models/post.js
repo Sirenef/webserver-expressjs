3cb425ec55f32d77a674012bf4cfe7eedf9f5c9b
Sirenef <anhngocbui332@gmail.com>
2023-10-14 10:26:43 +0700

    feat:week_6

---
+++ 20110354/back_end/models/post.js
@@ -0,0 +1,23 @@
+const listpost = [
+  {
+    id: 1,
+    content: "Post1",
+    author: "Anh",
+    listcomment: [
+      { id: 1, content: "Hi" },
+      { id: 2, content: "Hi" },
+    ],
+  },
+  {
+    id: 2,
+    content: "Post2",
+    author: "Ngien",
+    listcomment: [
+      { id: 1, content: "Hello" },
+      { id: 2, content: "Hi" },
+    ],
+  },
+];
+module.exports = {
+  listpost,
+};

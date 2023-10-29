9c1489b6f085368721aad9fe9bd522432e70ed30
Sirenef <anhngocbui332@gmail.com>
2023-10-30 04:57:45 +0700

    feat:week 7

---
+++ Nasa_20110354/client/src/hooks/requests.js
@@ -0,0 +1,44 @@
+const API_URL = "http://localhost:8000";
+async function httpGetPlanets() {
+  const response = await fetch(`${API_URL}/planets`);
+  return await response.json();
+}
+
+async function httpGetLaunches() {
+  const response = await fetch(`${API_URL}/launches`);
+  const fectchedLaunches = await response.json();
+  return fectchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
+}
+
+async function httpSubmitLaunch(launch) {
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(launch),
+    });
+  } catch (error) {
+    return { ok: false, message: error.message };
+  }
+}
+
+async function httpAbortLaunch(id) {
+  // TODO: Once API is ready.
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: "delete",
+    });
+  } catch (err) {
+    console.log(err);
+    return {
+      ok: false,
+    };
+  }
+  // Delete launch with given ID.
+}
+
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
